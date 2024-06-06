5166998b996831ebfaf554997bea5f1492e12303
alexay7 <alexay7@gmail.com>
2024-06-06 19:57:07 +0200

    Add rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,28 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { LogSchema } from 'src/schemas/log.schema';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI as string),
     MongooseModule.forFeature([{ name: 'Log', schema: LogSchema }]),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
